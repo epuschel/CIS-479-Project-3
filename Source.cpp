171a82dba531d9460cb4ff019e2604e67212650e
epuschel <73982403+epuschel@users.noreply.github.com>
2020-12-01 21:17:13 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -118,42 +118,7 @@ Node* locate(struct Node* node, string s, bool a) {
 	}
 	return NULL;
 }
-void fillGrid(struct Node* head) {
-	
-	struct Node* goal;
-	string msg = "";
-	int counter = 1;
 
-	for (int i = 0; i < 30; i++) {
-
-		if (i < 5)
-			msg = "a";
-		if (i >= 5 && i < 10)
-			msg = "b";
-		if (i >= 10 && i < 15)
-			msg = "c";
-		if (i >= 15 && i < 20)
-			msg = "d";
-		if (i >= 20 && i < 25)
-			msg = "e";
-		if (i >= 25 && i < 30)
-			msg = "f";
-
-
-		if (i == 6 || i == 7 || i == 11 || i == 16 || i == 17 || i == 21) {
-			append(&head, -1, msg + to_string(counter), true);
-		}
-		else
-			append(&head, 0.0, msg + to_string(counter), false);
-		if (counter == 5) {
-			counter = 1;
-		}
-		else
-			counter++;
-	}
-	goal = locate(head, "c3");
-	goal->data = 100;
-}
 // AUTHOR: Christopher Bourn
 // CREATION DATE: 
 // LAST MODIFIED:
@@ -179,6 +144,47 @@ void reConnectGrid(struct Node* head, struct Node* node) {
 		last = node;
 		node = node->next;
 	}
+}
+
+// AUTHOR: Christopher Bourn
+// CREATION DATE: 
+// LAST MODIFIED:
+// INPUT: 
+// OUTPUT: 
+// DESCRIPTION: 2nd function finds the node above or below a node
+void fillGrid(struct Node** head) {
+
+	struct Node* goal;
+	string msg = "";
+	int counter = 1;
+
+	for (int i = 0; i < 30; i++) {
+
+		if (i < 5)
+			msg = "a";
+		if (i >= 5 && i < 10)
+			msg = "b";
+		if (i >= 10 && i < 15)
+			msg = "c";
+		if (i >= 15 && i < 20)
+			msg = "d";
+		if (i >= 20 && i < 25)
+			msg = "e";
+		if (i >= 25 && i < 30)
+			msg = "f";
+
+		if (i == 6 || i == 7 || i == 11 || i == 16 || i == 17 || i == 21)
+			append((head), -1, msg + to_string(counter), true);
+		else
+			append((head), 0.0, msg + to_string(counter), false);
+		if (counter == 5)
+			counter = 1;
+		else
+			counter++;
+	}
+	reConnectGrid(*head, *head);
+	goal = locate(*head, "c3");
+	goal->data = 100;
 }
 
 // AUTHORS: Christopher Bourn & Ethan Puschell
@@ -404,10 +410,8 @@ Node* RandomStart(Node* head)
 int main() {
 	srand((int)time(0));
 	Node* head = NULL;
-	string msg = "";
+	/*string msg = "";
 	int counter = 1;
-
-	/*Fill grid*/
 	for (int i = 0; i < 30; i++) {
 
 		if (i < 5)
@@ -422,8 +426,6 @@ int main() {
 			msg = "e";
 		if (i >= 25 && i < 30)
 			msg = "f";
-
-
 		if (i == 6 || i == 7 || i == 11 || i == 16 || i == 17 || i == 21) {
 			append(&head, -1, msg + to_string(counter), true);
 		}
@@ -434,11 +436,11 @@ int main() {
 		}
 		else
 			counter++;
-	}
+	}*/
 	
 	/*Make the doubly linked list a quadruply linked list by connecting the north and south nodes*/
-	//fillGrid(head);
-	reConnectGrid(head, head);
+	fillGrid(head);
+	//reConnectGrid(head, head);
 	printKey(head);
 	cout << endl << endl;
 	printNList(head);
